4f1d7c78119f6f8672cad8bc7c331e9c045fa3d1
raj2577 <112565295+raj2577@users.noreply.github.com>
2022-09-10 20:13:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name')
+var desc = document.querySelector('.desc')
+var temp = document.querySelector('.temp')
+
+button.addEventListener('click',function(){
+    
+fetch ('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=f8b3eb57cb96a4646d57d784b127f8ae')
+.then(Response => Response.json())
+.then(data => {
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather']['0']['description'];
+
+
+
+    name.innerHTML =nameValue;
+    temp.innerHTML =tempValue;
+    desc.innerHTML =descValue;
+    
+
+
+})
+
+.catch(err => alert("Wrong coty Name!")) 
+})
